694635107da8b5234d0d4e0c56aba111d5b236f7
Petr <peterpankratc@gmail.com>
2026-06-14 14:48:08 +0300

    finaly version

--- api/schemas.py
+++ api/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 class UserSchema(BaseModel):
     name: str
     password: str = Field( max_length=30)
+    model_config = {'extra': 'forbid'}   
 
 class TaskSchema(BaseModel):
     task: str
